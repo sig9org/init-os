31ac55d198266cc473841777619f5a33ad49facf
sig9 <sig9@sig9.org>
2024-08-10 21:11:58 +0900

    Enable /var/log/messages.

--- init-linux.sh
+++ init-linux.sh
@@ -22,6 +22,74 @@ StrictHostKeyChecking no
 UserKnownHostsFile=/dev/null
 EOF
 
+  # Enable /var/log/messages.
+  cat << 'EOF' > /etc/rsyslog.d/50-default.conf
+#  Default rules for rsyslog.
+#
+#   For more information see rsyslog.conf(5) and /etc/rsyslog.conf
+
+#
+# First some standard log files.  Log by facility.
+#
+auth,authpriv.*   /var/log/auth.log
+*.*;auth,authpriv.none  -/var/log/syslog
+#cron.*    /var/log/cron.log
+#daemon.*   -/var/log/daemon.log
+kern.*    -/var/log/kern.log
+#lpr.*    -/var/log/lpr.log
+mail.*    -/var/log/mail.log
+#user.*    -/var/log/user.log
+
+#
+# Logging for the mail system.  Split it up so that
+# it is easy to write scripts to parse these files.
+#
+#mail.info   -/var/log/mail.info
+#mail.warn   -/var/log/mail.warn
+mail.err   /var/log/mail.err
+
+#
+# Some "catch-all" log files.
+#
+#*.=debug;\
+# auth,authpriv.none;\
+# news.none;mail.none -/var/log/debug
+*.=info;*.=notice;*.=warn;\
+ auth,authpriv.none;\
+ cron,daemon.none;\
+ mail,news.none  -/var/log/messages
+
+#
+# Emergencies are sent to everybody logged in.
+#
+*.emerg    :omusrmsg:*
+
+#
+# I like to have messages displayed on the console, but only on a virtual
+# console I usually leave idle.
+#
+#daemon,mail.*;\
+# news.=crit;news.=err;news.=notice;\
+# *.=debug;*.=info;\
+# *.=notice;*.=warn /dev/tty8
+EOF
+
+  cat << 'EOF' > /etc/logrotate.d/syslog
+/var/log/messages
+{
+ rotate 4
+ weekly
+ missingok
+ notifempty
+ compress
+ delaycompress
+ sharedscripts
+ postrotate
+  /usr/lib/rsyslog/rsyslog-rotate
+ endscript
+}
+EOF
+
   # Disable AppArmor
   systemctl stop apparmor.service
   systemctl disable apparmor.service
